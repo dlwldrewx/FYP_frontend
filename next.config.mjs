d54a164796cda89a35fcf84e1ee96c246dd4e381
dlwldrewx <dlwldrewx@1utar.my>
2025-03-10 20:55:43 +0800

    fixes next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
           {
             source: '/api/:path*', // Matches API requests in the Next.js app
-            destination: 'http://localhost:5000/api/:path*', // Redirects to the backend server
+            destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`, // Redirects to the backend server
           },
         ];
       },
